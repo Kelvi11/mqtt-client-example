1d5d26411227954591b72daf4deb340539ceaf4b
dervishllari <kelvin.dervishllari@moveoalbania.com>
2022-11-03 10:34:08 +0100

    structure

---
+++ src/main/java/com/example/mqttclientexample/MqttClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqttclientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttClientExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mqttclientexample/MqttClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
